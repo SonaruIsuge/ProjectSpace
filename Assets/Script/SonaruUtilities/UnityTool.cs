0d3315838e564920594e611b1c9c115c3fa36855
Sonaru <bread511206@gmail.com>
2023-02-27 12:48:39 +0900

    [Add] My common used utilities

---
+++ Assets/Script/SonaruUtilities/UnityTool.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+namespace SonaruUtilities
+{
+    public static class UnityTool
+    {
+        public static Rect RectTransformToScreenSpace(RectTransform transform)
+        {
+            Vector2 size = Vector2.Scale(transform.rect.size, transform.lossyScale);
+            Rect rect = new Rect(transform.position.x, Screen.height - transform.position.y, size.x, size.y);
+            rect.x -= (transform.pivot.x * size.x);
+            rect.y -= ((1.0f - transform.pivot.y) * size.y);
+            return rect;
+        }
+
+
+        public static Vector2 GetOrthographicCameraWorldSize(Camera camera)
+        {
+            if (!camera.orthographic) return Vector2.zero;
+
+            var height = camera.orthographicSize * 2.0f;
+            return new Vector2(camera.aspect * height, height);
+        }
+    }
+}
