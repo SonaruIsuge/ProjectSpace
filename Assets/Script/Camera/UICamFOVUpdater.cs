ca0403dbb4f1be8b9ae4622e53098423994bc635
Sonaru <bread511206@gmail.com>
2023-04-12 15:28:17 +0900

    [Fix] World UI position not correct bug

---
+++ Assets/Script/Camera/UICamFOVUpdater.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Camera))]
+public class UICamFOVUpdater : MonoBehaviour
+{
+    [SerializeField] private Camera targetCam;
+    private Camera uICam;
+
+
+    private void Awake()
+    {
+        uICam = GetComponent<Camera>();
+    }
+
+
+    private void Update()
+    {
+        uICam.fieldOfView = targetCam.fieldOfView;
+    }
+}
