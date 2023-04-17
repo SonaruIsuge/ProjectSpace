2f8a411d945b0f00e73eaef19fb7d45040cf6ba2
Sonaru <bread511206@gmail.com>
2023-04-17 12:18:02 +0900

    [Fix] Build bug

--- Assets/Script/SonaruUtilities/EditorTool.cs
+++ Assets/Script/SonaruUtilities/EditorTool.cs
@@ -8,6 +8,7 @@ namespace SonaruUtilities
     {
         public static void DrawUILine(Color color = default, int thickness = 1, int padding = 10, int margin = 0)
         {
+#if UNITY_EDITOR
             color = color != default ? color : Color.grey;
             var r = EditorGUILayout.GetControlRect(false, GUILayout.Height(padding + thickness));
             r.height = thickness;
@@ -30,6 +31,7 @@ namespace SonaruUtilities
             }
  
             EditorGUI.DrawRect(r, color);
+#endif
         }
     }
 }
