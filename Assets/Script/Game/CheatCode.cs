d698663dc6b511af0a4243b5f19a6f194e3cca67
Sonaru <bread511206@gmail.com>
2023-04-17 12:47:06 +0900

    [Add] Cheat code for demo

---
+++ Assets/Script/Game/CheatCode.cs
@@ -0,0 +1,36 @@
+﻿using SonaruUtilities;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+
+public class CheatCode : MonoBehaviour
+{
+    private ItemManager itemManager;
+    private SimpleTimer cheatPressTimer;
+
+    private bool cheatUsed;
+
+    private void Awake()
+    {
+        itemManager = FindObjectOfType<ItemManager>();
+        
+        cheatPressTimer = new SimpleTimer(3);
+        cheatPressTimer.Pause();
+
+        cheatUsed = false;
+    }
+    
+
+    private void Update()
+    {
+        if (cheatUsed) return;
+        
+        if (Keyboard.current.kKey.isPressed) cheatPressTimer.Resume();
+        else cheatPressTimer.Reset();
+
+        if (!cheatPressTimer.IsFinish) return;
+        itemManager.ClearAllItem();
+        cheatPressTimer.Pause();
+        cheatUsed = true;
+    }
+}
