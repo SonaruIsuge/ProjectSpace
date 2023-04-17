94a4a8e182bf2f185261910bd742b31d30c51441
Sonaru <bread511206@gmail.com>
2023-04-17 10:53:34 +0900

    [Update] New enter tutorial button image

---
+++ Assets/Script/Scene_Pair/UI/ShowIcon.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+
+public class ShowIcon : MonoBehaviour
+{
+    [SerializeField] private Button yesBtn;
+    [SerializeField] private Button noBtn;
+    [SerializeField] private RawImage yesIcon;
+    [SerializeField] private RawImage noIcon;
+        
+    private EventSystem EventSystem => EventSystem.current;
+    
+    
+    private void Update()
+    {
+        if (EventSystem.currentSelectedGameObject == yesBtn.gameObject)
+        {
+            yesIcon.gameObject.SetActive(true);
+            noIcon.gameObject.SetActive(false);
+        }
+
+        if (EventSystem.currentSelectedGameObject == noBtn.gameObject)
+        {
+            yesIcon.gameObject.SetActive(false);
+            noIcon.gameObject.SetActive(true);
+        }
+    }
+}
